9b6c16f50d2dd55f86a956ca3fda1e96d9f1db6d
Travis Blankenship <tblank555@gmail.com>
2014-03-27 11:32:02 -0700

    Added the ability to continue monitoring a file even if another app deletes and re-creates it.

--- File Monitor/TABViewController.m
+++ File Monitor/TABViewController.m
@@ -12,6 +12,8 @@
 {
     NSURL *_testFileURL;
     dispatch_source_t _source;
+    int _fileDescriptor;
+    BOOL _keepMonitoringFile;
     __weak IBOutlet UITextField *_textToWriteField;
     __weak IBOutlet UITextView *_eventTextView;
 }
@@ -26,6 +28,9 @@
 {
     [super viewDidLoad];
     
+    // Reset this flag. It allows us to keep monitoring a file even if another app deletes and re-creates it.
+    _keepMonitoringFile = NO;
+    
     // Create the test file URL
     NSURL *documentsDirectory = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
                                                                         inDomains:NSUserDomainMask] firstObject];
@@ -35,17 +40,29 @@
     [self __writeText:@"Whadderp?"
                 toURL:_testFileURL];
     
+    [self __beginMonitoringFile];
+}
+
+- (void)dealloc
+{
+    dispatch_source_cancel(_source);
+}
+
+#pragma mark - Private Methods
+
+- (void)__beginMonitoringFile
+{
     // Add a file descriptor for our test file
-    int fileDescriptor = open([[_testFileURL path] fileSystemRepresentation],
-                              O_EVTONLY);
+    _fileDescriptor = open([[_testFileURL path] fileSystemRepresentation],
+                           O_EVTONLY);
     
-    // Create a GCD queue to receive file change event notifications on
-    dispatch_queue_t eventQueue = dispatch_queue_create("Filesystem Event Queue", 0);
+    // Get a reference to the default queue so our file notifications can go out on it
+    dispatch_queue_t defaultQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     
     _source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE,
-                                     fileDescriptor,
+                                     _fileDescriptor,
                                      DISPATCH_VNODE_ATTRIB | DISPATCH_VNODE_DELETE | DISPATCH_VNODE_EXTEND | DISPATCH_VNODE_LINK | DISPATCH_VNODE_RENAME | DISPATCH_VNODE_REVOKE | DISPATCH_VNODE_WRITE,
-                                     eventQueue);
+                                     defaultQueue);
     
     // Log one or more messages to the screen when there's a file change event
     dispatch_source_set_event_handler(_source, ^
@@ -60,7 +77,10 @@
         if (eventType & DISPATCH_VNODE_LINK)
             [self __logTextToScreen:@"Test file's object link count changed."];
         if (eventType & DISPATCH_VNODE_RENAME)
+        {
             [self __logTextToScreen:@"Test file was renamed."];
+            [self __resetFileDescriptor];
+        }
         if (eventType & DISPATCH_VNODE_REVOKE)
             [self __logTextToScreen:@"Test file was revoked."];
         if (eventType & DISPATCH_VNODE_WRITE)
@@ -70,15 +90,24 @@
     
     dispatch_source_set_cancel_handler(_source, ^
     {
-        close(fileDescriptor);
+        close(_fileDescriptor);
+        _fileDescriptor = 0;
+        _source = nil;
+        
+        if (_keepMonitoringFile)
+        {
+            _keepMonitoringFile = NO;
+            [self __beginMonitoringFile];
+        }
     });
     
     // Start monitoring the test file
     dispatch_resume(_source);
 }
 
-- (void)dealloc
+- (void)__resetFileDescriptor
 {
+    _keepMonitoringFile = YES;
     dispatch_source_cancel(_source);
 }
 
@@ -96,7 +125,7 @@
     [_textToWriteField resignFirstResponder];
 }
 
-#pragma mark - Helper Methods
+#pragma mark - UI Helper Methods
 
 - (void)__writeText:(NSString *)text toURL:(NSURL *)URL
 {
@@ -126,7 +155,7 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    [self dismissKeyboard:nil];
+    [self write:nil];
     return YES;
 }
 
